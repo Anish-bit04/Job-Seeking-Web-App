a63c76c80034e4fd39e9a2ddaf1dc4c163e0e34c
Anish <work.anish.004@gmail.com>
2024-07-30 20:14:16 +0530

    connected frontend and backend , run on single port

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ import { dbConnection } from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import path from 'path'
 
 const app = express();
-config({ path: "./config/config.env" });
+const __dirname = path.resolve()
+dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
@@ -35,6 +37,12 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 dbConnection();
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'/frontend/dist/index.html'))
+})
 
 app.use(errorMiddleware);
 export default app;
